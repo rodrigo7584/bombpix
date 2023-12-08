156f5c8b528239b937023c71e134ef564fc6bc7f
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-12-08 11:02:49 -0300

    08/12/2023

--- header.php
+++ header.php
@@ -5,6 +5,28 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+  <!-- Primary Meta Tags -->
+  <title>BOMB PIX - Detone. enriqueça. domine. </title>
+  <meta name="title" content="BOMB PIX - Detone. enriqueça. domine. " />
+  <meta name="description" content="O jogo explosivo que transforma suas vitórias em riquezas reais!" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://bombpix.net/" />
+  <meta property="og:title" content="BOMB PIX - Detone. enriqueça. domine. " />
+  <meta property="og:description" content="O jogo explosivo que transforma suas vitórias em riquezas reais!" />
+  <meta property="og:image" content="./img/logo.png" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="http://bombpix.net/" />
+  <meta property="twitter:title" content="BOMB PIX - Detone. enriqueça. domine. " />
+  <meta property="twitter:description" content="O jogo explosivo que transforma suas vitórias em riquezas reais!" />
+  <meta property="twitter:image" content="./img/logo.png" />
+
+  <!-- Meta Tags Generated with https://metatags.io -->
+
+
   <link rel="icon" type="image/png" href="./img/favicon-32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="./img/favicon-16.png" sizes="16x16" />
 
